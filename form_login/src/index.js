3bf0e5c720c522cd5894e56ff9ae2afe812459dd
arip1997 <ariefr.santoso@gmail.com>
2020-02-27 16:37:21 +0700

    Form dan Button

--- form_login/src/index.js
+++ form_login/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import HelloComponent from './components/HelloComponent';
 import FormLogin from './components/FormLogin';
+import * as serviceWorker from './serviceWorker';
 
+class Statefullcomponent extends React.Component{
+    render(){
+        return <p>ini adalah statefull component</p>
+    }
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<FormLogin />, document.getElementById('root'));
 ReactDOM.render(<FormLogin />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
